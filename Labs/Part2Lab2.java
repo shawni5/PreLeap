f92be761e1014bea3b28d0c2d054c9f56a6ac1bf
Shawni McMichael <shawnimcmichael@att.net>
2023-11-20 18:24:39 -0700

    Part2Lab2

---
+++ Labs/Part2Lab2.java
@@ -0,0 +1,22 @@
+public class Part2Lab2 {
+    public static void main(String[] args){
+        System.out.println("3");
+        System.out.println("2");
+        System.out.println("1");
+        System.out.println("Launch.....");
+        System.out.println("    ..    ");
+        System.out.println("   .--.   ");
+        System.out.println("  |----|  ");
+        System.out.println("  |NASA|  ");
+        System.out.println("  |----|  ");
+        System.out.println("  |----|  ");
+        System.out.println("  |----|  ");
+        System.out.println(" |------| ");
+        System.out.println(" |------| ");
+        System.out.println(" |______| ");
+        System.out.println("    ..    ");
+        System.out.println("   ....   ");
+        System.out.println("  ......  ");
+
+    }
+}
