9ccd47df82bcbadbddfa2c3ffc81a7de0040acb7
Shawni McMichael <shawnimcmichael@att.net>
2023-11-20 20:39:49 -0700

    finish Part3Lab1

---
+++ Labs/Part3Lab3.java
@@ -0,0 +1,12 @@
+public class Part3Lab3 {
+    public static void main(String[] args) {
+        int age = 57;
+        double weight = 125;
+        boolean isStudent = true;
+        String firstName = "Shawni";
+        System.out.println("Age: " + age);
+        System.out.println("Weight: " + weight);
+        System.out.println("Is a Student: " + isStudent);
+        System.out.println("First Name: " + firstName);
+    }
+}
