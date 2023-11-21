a8cdd37e7075587cbb48ab43c87352949c3afba4
Shawni McMichael <shawnimcmichael@att.net>
2023-11-21 11:25:30 -0700

    .

---
+++ Labs/part4Lab1.java
@@ -0,0 +1,7 @@
+public class part4Lab1 {
+    public static void main(String[] args) {
+        double num1 = 10, num2 = 20, num3 = 30, num4 = 40, num5 = 50;
+        double average = (num1 + num2 + num3 + num4 + num5) / 5;
+       System.out.println("The average of 10, 20, 30, 40 and 50 is: " + average);
+    }
+}
