16d0c71e68524a58492d85b70d0865160d340bd4
Shawni McMichael <shawnimcmichael@att.net>
2023-12-02 12:06:31 -0700

    finish Part9Lab2

---
+++ Labs/Part9Lab2.java
@@ -0,0 +1,15 @@
+public class Part9Lab2 {
+    public static void sayHello(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    public static void sayGoodbye(String name) {
+        System.out.println("Goodbye, " + name);
+    }
+
+    public static void main(String[] args) {
+        String name = "Shawni!";
+        sayHello(name);
+        sayGoodbye(name);
+    }
+}
